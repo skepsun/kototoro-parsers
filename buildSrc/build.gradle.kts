f87f9b27237ced7da73f6f793c0daa6eaf776fef
skepsun <chuxiongsun@gmail.com>
2026-04-09 15:28:12 +0800

    chore: remove aliyun maven mirrors to fix 502 Bad Gateway

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,12 +3,9 @@ plugins {
 }
 
 repositories {
-    // 优先使用国内镜像以避免 TLS 握手问题
-    maven("https://maven.aliyun.com/repository/gradle-plugin")
-    maven("https://maven.aliyun.com/repository/public")
     google()
-    maven("https://maven.aliyun.com/repository/google")
     mavenCentral()
+    gradlePluginPortal()
 }
 
 // 移除 jvmToolchain 限制，使用系统默认JDK
